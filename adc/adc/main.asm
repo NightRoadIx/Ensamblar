68101bc8409ef6130d976858cfb3ebd496de0410
NightRoadIx <quintanarxbio@gmail.com>
2018-05-07 23:01:55 -0600

    Update main.asm

--- adc/adc/main.asm
+++ adc/adc/main.asm
@@ -8,7 +8,7 @@
 ; Incluir definiciones para el ATMEGA328P
 .INCLUDE "m328pdef.inc"
 
-.ORG 0x0000					; Organiza todo a partir de la direcciÛn 0x0000
+.ORG 0x0000					; Organiza todo a partir de la direcci√≥n 0x0000
 RJMP setup					; Asegurar que el programa comience en la etiqueta setup
 
 ; ******************************************************************************************************
@@ -32,36 +32,36 @@ setup:
 
 loop:
 	NOP
-; Iniciar la configuraciÛn del ADC
+; Iniciar la configuraci√≥n del ADC
 setADC:
 	CLR r16
 	; Modificar el registro de control y estado del ADC
 	; Iniciar en cero
 	STS ADCSRA, r16
 
-	LDI r16, 0b01000011
-	; Registro de selecciÛn del multiplexor del ADC
+	LDI r16, 0b01100011
+	; Registro de selecci√≥n del multiplexor del ADC
 	; Utilizar la referencia por AVcc, ajustar los datos a la izquierda, utilizar la entrada ADC3 (PORTC3)
 	STS ADMUX, r16
 
 	LDI r16, 0b11000111
-	; Configurar: Habilitar el ADC, Iniciar una conversiÛn, Preescaler para el reloj del ADC a 128
+	; Configurar: Habilitar el ADC, Iniciar una conversi√≥n, Preescaler para el reloj del ADC a 128
 	STS ADCSRA, r16
 
 verificar:
 	LDS r16, ADCSRA
-	SBRS r16, 4		; Esperar a que la conversiÛn se complete, cuando esto sucede el bit4 de ADCSRA se coloca en 1
+	SBRS r16, 4		; Esperar a que la conversi√≥n se complete, cuando esto sucede el bit4 de ADCSRA se coloca en 1
 	JMP verificar
 
-	LDS r17, ADCL	; Cargar la parte baja del valor de la conversiÛn
-	; Cargar la parte alta del valor de la conversiÛn, como est· ajustado a la izquierda, solo hay una precisiÛn de 8 bits y solo es requerido leer este
-	; Si ADLAR de ADCSRA no est· colocado, entonces los valores se ajustan a la derecha y el valor se guarda en ADCH:ADCL
+	LDS r17, ADCL	; Cargar la parte baja del valor de la conversi√≥n
+	; Cargar la parte alta del valor de la conversi√≥n, como est√° ajustado a la izquierda, solo hay una precisi√≥n de 8 bits y solo es requerido leer este
+	; Si ADLAR de ADCSRA no est√° colocado, entonces los valores se ajustan a la derecha y el valor se guarda en ADCH:ADCL
 	LDS r18, ADCH
 
-	OUT PORTB, r17		; Mandar al PORTB lo que se lee en el ADC
-	BST r17, 6
+	OUT PORTB, r18		; Mandar al PORTB lo que se lee en el ADC
+	BST r18, 6
 	BLD r19, 7 
-	BST r17, 7
+	BST r18, 7
 	BLD r19, 5
 	OUT PORTD, r19
 	RCALL delay_05
@@ -78,7 +78,7 @@ outer_loop:
 	LDI r24, low(3037)		; Cargar low(3037) en el registro 24
 	LDI r25, high(3037)		; Cargar high(3037) en el registro 25
 	delay_loop: 
-		ADIW r24, 1				; AÒadir inmediatamente a la palabra, r24:r25 se incrementan
+		ADIW r24, 1				; A√±adir inmediatamente a la palabra, r24:r25 se incrementan
 		BRNE delay_loop			; si no hay un overflow ("salir si no es igual"), regresar a la etiqueta
 		DEC r16					; disminuir r16
 		BRNE outer_loop			; si no hay un overflow ("salir si no es igual"), regresar a la etiqueta
